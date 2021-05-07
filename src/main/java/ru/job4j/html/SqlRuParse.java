642e3917ee1d7d4f6c36e5c58c11850329479790
ZuewOleg <zuev_17@bk.ru>
2021-05-07 14:21:22 +0500

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#479263]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -14,19 +14,24 @@ public class SqlRuParse {
     }
 
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            // Ячейка с именем имеет аттрибут class=postslisttopic. jsoup может извлечь все элементы с этим аттрибутом
-            Element href = td.child(0);
-            /* Elements data = doc.getElementsByClass("forumTable").get(0).getElementsByTag("tr"); */
-            Element data = td.parent();
-            LocalDateTime ld = parse(data.child(5).text());
-            // Нам нужен первый элемент. Это ссылка. У нее мы получаем адрес, текст и дату обновления.
-            System.out.println(href.attr("href")); // ссылка
-            System.out.println(href.text()); // текст
-            /*System.out.println(doc.getElementsByTag("tr").get(5).text()); // дата */
-            System.out.println(ld);
+        for (int i = 1; i <= 5; i++) { // цикл чтобы парсить 5 страниц
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get(); // url: ".../" + i,
+            // полсе адреса ставим "/", чтобы перейти на адрес страницы и прибавляем i
+            // было "https://www.sql.ru/forum/job-offers", стало "https://www.sql.ru/forum/job-offers/" + i
+            // где i количесиво нужных сраниц, т.е. страниц до которых нужно парсить
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                // Ячейка с именем имеет аттрибут class=postslisttopic. jsoup может извлечь все элементы с этим аттрибутом
+                Element href = td.child(0);
+                /* Elements data = doc.getElementsByClass("forumTable").get(0).getElementsByTag("tr"); */
+                Element data = td.parent();
+                LocalDateTime ld = parse(data.child(5).text());
+                // Нам нужен первый элемент. Это ссылка. У нее мы получаем адрес, текст и дату обновления.
+                System.out.println(href.attr("href")); // ссылка
+                System.out.println(href.text()); // текст
+                /*System.out.println(doc.getElementsByTag("tr").get(5).text()); // дата */
+                System.out.println(ld);
+            }
         }
     }
 }
