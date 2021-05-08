88abb39623fb610e17f38be13d170e29e2ba8aac
ZuewOleg <zuev_17@bk.ru>
2021-05-08 15:25:58 +0500

    3. Архитектура проекта - Аргегатора Java Вакансий [#479260]

--- src/main/java/ru/job4j/grabber/Parse.java
+++ src/main/java/ru/job4j/grabber/Parse.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.util.List;
 
 public interface Parse {
-    List<Post> list(String link) throws IOException;
+    List<Post> list(String link) throws IOException; // загружает список объявлений по ссылке
 
-    Post detail(String link) throws IOException;
+    Post detail(String link) throws IOException; // загружает детали объявления по ссылке
 }
---
+++ src/main/java/ru/job4j/grabber/Store.java
@@ -0,0 +1,14 @@
+package ru.job4j.grabber;
+
+import java.util.List;
+
+/** Метод save() - сохраняет объявление в базе
+ * Метод getAll() - позволяет извлечь объявления из базы
+ * Метод findById(String id) - позволяет извлечь объявление из базы по id */
+public interface Store {
+    void save(Post post);
+
+    List<Post> getAll();
+
+    Post findById(String id);
+}
