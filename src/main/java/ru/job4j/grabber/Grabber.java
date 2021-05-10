756934537167e819cdbbe0648c3a837d10b26df9
ZuewOleg <zuev_17@bk.ru>
2021-05-10 17:44:00 +0500

    7. Web. [#479269]

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -5,6 +5,8 @@ import org.quartz.impl.StdSchedulerFactory;
 import ru.job4j.html.SqlFullParse;
 
 import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Properties;
@@ -68,6 +70,27 @@ public class Grabber implements Grab {
         }
     }
 
+    public void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(Integer.parseInt(cfg.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+                            out.write(post.toString().getBytes());
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
 
     public static void main(String[] args) throws Exception {
         Grabber grab = new Grabber();
@@ -75,5 +98,6 @@ public class Grabber implements Grab {
         Scheduler scheduler = grab.scheduler();
         Store store = grab.store();
         grab.init(new SqlFullParse(), store, scheduler);
+        grab.web(store);
     }
 }
