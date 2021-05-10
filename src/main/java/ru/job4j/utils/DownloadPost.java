f4abc0b62fa417ce5380e9bce3f799ab9c151e79
ZuewOleg <zuev_17@bk.ru>
2021-05-10 13:47:01 +0500

    2.3. Загрузка деталей поста. [#479265]

--- src/main/java/ru/job4j/utils/DownloadPost.java
+++ src/main/java/ru/job4j/utils/DownloadPost.java
@@ -16,23 +16,23 @@ public class DownloadPost {
         Document doc = Jsoup.connect(
                 "https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t"
         ).get();
-        Elements row = doc.select(".msgBody");
+        Elements row = doc.select(".postslisttopic");
         for (Element td : row) {
             // Ячейка с именем имеет аттрибут class=msgBody. jsoup может извлечь все элементы с этим аттрибутом
             Element href = td.child(0);
             /* Elements data = doc.getElementsByClass("forumTable").get(0).getElementsByTag("tr"); */
-            Element data = td.parent();
-            LocalDateTime ld = parse(data.child(5).text());
-            // Нам нужен первый элемент. Это ссылка. У нее мы получаем адрес, текст и дату обновления.
-            Post post = new Post.Builder()
-                    .builderLink(href.attr("href"))
-                    .builderTopicName(data.child(1).child(0).text())
-                    .builderAuthor(data.child(2).text())
-                    .builderDescription(row.get(1).text())
-                    .builderReplies(Integer.parseInt(data.child(3).text()))
-                    .builderViews(Integer.parseInt(data.child(4).text()))
-                    .builderPostDate(ld).build();
-            System.out.println(post);
+                Element data = td.parent();
+                LocalDateTime ld = parse(data.child(5).text());
+                // Нам нужен первый элемент. Это ссылка. У нее мы получаем адрес, текст и дату обновления.
+                Post post = new Post.Builder()
+                        .builderLink(href.attr("href"))
+                        .builderTopicName(data.child(1).child(0).text())
+                        .builderAuthor(data.child(2).text())
+                        .builderDescription(row.get(1).text())
+                        .builderReplies(Integer.parseInt(data.child(3).text()))
+                        .builderViews(Integer.parseInt(data.child(4).text()))
+                        .builderPostDate(ld).build();
+                System.out.println(post);
         }
     }
 }
