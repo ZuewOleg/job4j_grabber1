2c6cb55ee5b20f4bfd931f3ea2a7038340f256c0
ZuewOleg <zuev_17@bk.ru>
2021-05-11 15:49:57 +0500

    SQL Select by company [#479286]

--- scripts/examSQL/SelectByCompany.sql
+++ scripts/examSQL/SelectByCompany.sql
@@ -21,7 +21,7 @@ insert into company(id, name) values (6, 'Honda');
 insert into person(id, name, company_id) values (1, 'Ivan', 1);
 insert into person(id, name, company_id) values (2, 'Max', 1);
 insert into person(id, name, company_id) values (3, 'Fedor', 1);
-insert into person(id, name, company_id) values (4, 'Petr', 2);
+insert into person(id, name, company_id) values (4, 'Petr', 1);
 insert into person(id, name, company_id) values (5, 'Nick', 2);
 insert into person(id, name, company_id) values (6, 'Alex', 3);
 insert into person(id, name, company_id) values (7, 'Ben', 3);
@@ -36,11 +36,7 @@ insert into person(id, name, company_id) values (15, 'Satoshi', 6);
 insert into person(id, name, company_id) values (16, 'Hanzo', 6);
 
 -- имена всех person, которые не состоят в компании с id = 5 --
-select p.name as "Не состоят в 5 компании" from person p where company_id != 5;
 select * from person where company_id != 5;
-select p.name as "Имя сотрудника", c.name as "Название компании", c.id "Номер компании"
-from company c left join person p on c.id = p.company_id
-where p.company_id != 5;
 
 -- название компании для каждого человека --
 select c.name as "Название компании", p.name as "Имя сотрудника"
@@ -50,4 +46,5 @@ from company c join person p on c.id = p.company_id;
 select c.name, count(p) from person p
 join company c on c.id = p.company_id
 group by c.name
-order by count(p);
+order by count(p) desc
+fetch first row only;
