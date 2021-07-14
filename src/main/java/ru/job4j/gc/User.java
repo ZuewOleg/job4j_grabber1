a699319a1a3d81d2a9c2641856dc609c4efb28ae
ZuewOleg <zuev_17@bk.ru>
2021-07-14 19:12:30 +0500

    1. Демонстрация работы GC [#1589 #177614]

---
+++ src/main/java/ru/job4j/gc/User.java
@@ -0,0 +1,35 @@
+package ru.job4j.gc;
+
+public class User {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public User() {
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.printf("Removed %d %s%n", age, name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
